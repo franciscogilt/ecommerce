2587c1e5cac031c15bb4d950c2120c482816afe2
Francisco Gil <franciscogilt@gmail.com>
2021-04-13 13:20:59 -0400

    Add card template and include it in others

--- auctions/models.py
+++ auctions/models.py
@@ -5,21 +5,25 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    
+
     def __str__(self):
         return self.name
+
 
 class Listing(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
     price = models.FloatField()
-    category = models.ForeignKey(Category, null=True, default=models.SET_NULL, on_delete=models.SET_NULL)
+    category = models.ForeignKey(
+        Category, null=True, default=models.SET_NULL, on_delete=models.SET_NULL)
     image = models.URLField(blank=True)
     date_added = models.DateTimeField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     is_active = models.BooleanField(default=True)
+
 
 class Bid(models.Model):
     amount = models.FloatField()
@@ -27,12 +31,15 @@ class Bid(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
 
+
 class Comment(models.Model):
     content = models.CharField(max_length=200)
     date_added = models.DateTimeField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
 
+
 class Watchlist(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="watchlist")
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="watchlist")
     listings = models.ManyToManyField(Listing, blank=True)
